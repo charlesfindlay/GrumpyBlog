c4d57c01578ec2db6f4e7f270d69c67b5cc13f73
Donovan Cotter <donovan.cotter@yahoo.com>
2015-10-09 12:03:58 -0400

    working view controller

--- GrumpyBlog/ViewController.swift
+++ GrumpyBlog/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var myBlog = [BlogEntry]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let blog = BlogEntry(title: "Blog One", content: "This is our first blog")
+        myBlog.append(blog)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myBlog.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "Default")
+        
+        
+        cell.textLabel?.text = myBlog[indexPath.row].title
+        
+        return cell
+    }
+    
+    
 }
+
+
 
